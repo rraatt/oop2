1be9f497cc9e156d5a2084f1ca2786702f12213b
rraatt <rraatt12321@gmail.com>
2021-09-30 11:48:56 +0300

    Initial commit

---
+++ 2_1.py
@@ -0,0 +1,32 @@
+class Rectangle:
+    def __init__(self):
+        self.__lenght = 0
+        self.__height = 0
+
+    def setter(self, a, b):
+        if (0 < a < 20) and (0 < b < 20):
+            # Check if values are in allowed range.
+            self.__lenght = a
+            self.__height = b
+        else:
+            print("Values are out of allowed range!")
+            exit(1)
+
+    def getter(self):
+        return self.__lenght, self.__height
+
+    def per(self):
+        return sum(self.getter())*2
+    # Returns sum of two sides multiplied by two.
+
+    def area(self):
+        var = self.getter()
+        return var[0]*var[1]
+    # Returns result of multiplication of two sides
+
+
+obj = Rectangle()
+obj.setter(10.1, 15)
+print(obj.getter())
+print(obj.per())
+print(obj.area())
---
+++ 2_2.py
@@ -0,0 +1,28 @@
+import math
+
+
+class Rational:
+
+    def __init__(self, *values):
+        if values:
+            if isinstance(values[0], int) and isinstance(values[1], int):
+                divisor = math.gcd(values[0], values[1])
+                self.__numerator = int(values[0] / divisor)
+                self.__denominator = int(values[1] / divisor)
+            else:
+                print("Values are not integers!")
+                exit(1)
+        else:
+            self.__numerator = 1
+            self.__denominator = 1
+
+    def get(self):
+        return str(self.__numerator) + '/' + str(self.__denominator)
+
+    def getfloat(self):
+        return self.__numerator/self.__denominator
+
+
+obj = Rational(2, 8)
+print(obj.get())
+print(obj.getfloat())
