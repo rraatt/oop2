9ef94fac7f7f71ceaedf91684171fbc2d098c913
rraatt <82118136+rraatt@users.noreply.github.com>
2021-10-06 20:44:01 +0300

    added ariphmetic actions

--- 2_2.py
+++ 2_2.py
@@ -21,7 +21,29 @@ class Rational:
     def getfloat(self):
         return self.__numerator/self.__denominator
 
+    def add(self, second):
+        self.__numerator = self.__numerator * second.__denominator + second.__numerator * self.__denominator
+        self.__denominator = self.__denominator * second.__denominator
+        return Rational(self.__numerator, self.__denominator)
+
+    def sub(self, second):
+        self.__numerator = self.__numerator * second.__denominator - second.__numerator * self.__denominator
+        self.__denominator = self.__denominator * second.__denominator
+        return Rational(self.__numerator, self.__denominator)
+
+
+    def mult(self, second):
+        self.__numerator = self.__numerator * second.__numerator
+        self.__denominator = self.__denominator * second.__denominator
+        return Rational(self.__numerator, self.__denominator)
+
+    def div(self, second):
+        self.__numerator = self.__numerator * second.__denominator
+        self.__denominator = self.__denominator * second.__numerator
+        return Rational(self.__numerator, self.__denominator)
 
 obj = Rational(2, 8)
+obj2 = Rational(3, 12)
+obj = obj.add(obj2)
 print(obj.get())
 print(obj.getfloat())
