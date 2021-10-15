c42203d11ce1773a124c64d3a609fba783764ae4
rraatt <rraatt12321@gmail.com>
2021-10-15 17:25:50 +0300

    fixed third and forth task

--- 2_3.py
+++ 2_3.py
@@ -2,12 +2,13 @@ class Product:
     __slots__ = ['name', 'price']
 
     def __init__(self, name, price):
-        if isinstance(name, str) and isinstance(price, int or float):
-            # Check for correct input.
-            self.name = name
-            self.price = price
-        else:
-            raise TypeError("Incorrect info for new product!")
+        if not isinstance(name, str):
+            raise TypeError("{Product name should be a string!")
+        if not isinstance(price, (int, float)):
+            raise TypeError("Price should be integer or float data type!")
+        # Check for correct input.
+        self.name = name
+        self.price = price
 
     def __str__(self):
         return f'{self.name} {self.price}'
@@ -17,6 +18,8 @@ class Customer:
     __slots__ = ['name', 'surname', 'mobile']
 
     def __init__(self, name, surname, mobile):
+        if not name or not surname or not mobile:
+            raise ValueError("Missing values in input!")
         self.name = name
         self.surname = surname
         self.mobile = mobile
@@ -27,32 +30,34 @@ class Customer:
 
 class Order:
     def __init__(self, customer):
-        if isinstance(customer, Customer):
-            # Check if the input is correct class object
-            self.info = customer.__str__()
-            # Save info about customer.
-            self.prod_list = []
-            self.total = 0
-        else:
+        if not isinstance(customer, Customer):
             raise TypeError("Argument for order should be Customer type!")
+        # Check if the input is correct class object
+        self.info = customer
+        # Save info about customer.
+        self.prod_list = []
 
     def add_item(self, product):
-        if isinstance(product, Product):
-            # Add an item to item list and ad up to order total
-            self.prod_list.append(product.name)
-            self.total += product.price
-        else:
+        if not isinstance(product, Product):
             raise TypeError("Argument for new item should be Product type!")
+        # Add an item to item list and ad up to order total
+        self.prod_list.append(product)
 
     def checkout(self):
-        return f'total for the order: {self.total} Items bought: {self.prod_list} Customer info: {self.info}'
+        total = sum([x.price for x in self.prod_list])
+        return f'total for the order: {total} Items bought: {[str(x) for x in self.prod_list]} ' \
+               f'Customer info: {str(self.info)}'
 
 
-John = Customer('John', 'Smith', '0123456789')
-Prod1 = Product('Cheese', 5)
-Prod2 = Product('Bread', 3)
+def main():
+    john = Customer('John', 'Smith', '0123456789')
+    prod1 = Product('Cheese', 5)
+    prod2 = Product('Bread', 3)
+    ord = Order(john)
+    ord.add_item(prod1)
+    ord.add_item(prod2)
+    print(ord.checkout())
 
-Ord = Order(John)
-Ord.add_item(Prod1)
-Ord.add_item(Prod2)
-print(Ord.checkout())
+
+if __name__ == '__main__':
+    main()
--- 2_4.py
+++ 2_4.py
@@ -1,34 +1,52 @@
 from collections import Counter
 import re
 from string import punctuation
+import os.path
 
 
 class TextProcessor:
-    __slots__ = ["raw", "words", "sentences", "filtered", "popular_list"]
+    __slots__ = ["file_name", "filtered", "popular_list"]
 
-    def __init__(self, file):
-        if not isinstance(file, str):
-            raise TypeError("Name of file required in input!!!")
-        self.raw = open(file).read()
+    def __init__(self, file_name):
+        if not os.path.isfile(file_name):
+            raise ValueError("File doesnt exist!")
+        self.file_name = file_name
         # Open text file for reading.
-        self.words = len(self.raw.split())
-        # Splitting the file for words counting
-        self.sentences = len(re.split(r"[.!?]+", self.raw))
-        # Splitting the file for sentences counting.
-        self.filtered = self.raw.translate(punctuation).lower().split()
-        # Splitting the text and removing punctuation to find most common words
-        count = Counter(self.filtered)
-        self.popular_list = [item for item in count.most_common(10)]
 
     def searcher(self, inp):
-        count = Counter(self.raw.lower())
-        return f'Number of occurrences of {inp} is {count[inp]}'
+        with open(self.file_name) as f:
+            text = f.read()
+            count = Counter(text.lower())
+            return f'Number of occurrences of {inp} is {count[inp]}'
+
+    def word_count(self):
+        with open(self.file_name) as f:
+            text = f.read()
+            words = len(text.split())
+        return words
+
+    def sentence_count(self):
+        with open(self.file_name) as f:
+            text = f.read()
+            sentences = len(re.split(r"[.!?]+", text))
+        return sentences
+
+    def most_common(self):
+        with open(self.file_name) as f:
+            text = f.read()
+            count = Counter(text.translate(punctuation).lower().split())
+        return [item for item in count.most_common(10)]
 
     def __str__(self):
-        return f'Number of words: {self.words} Number of sentences: {self.sentences}\n' \
-               f'Most popular words: {self.popular_list}'
+        return f'Number of words: {self.word_count()} Number of sentences: {self.sentence_count()}\n' \
+               f'Most popular words: {self.most_common()}'
 
 
-obj = TextProcessor('test.txt')
-print(obj)
-print(obj.searcher('b'))
+def main():
+    obj = TextProcessor('test.txt')
+    print(obj)
+    print(obj.searcher('b'))
+
+
+if __name__ == '__main__':
+    main()
